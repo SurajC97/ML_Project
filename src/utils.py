a49f1b7dcb29d7e39fd2cf1ceb5a6cbd39ee9178
SurajC97 <surajchakraborty6@gmail.com>
2023-08-16 23:59:47 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,9 @@ import os
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -15,3 +17,32 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test, models, param):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model= list(models.values())[i]
+
+            para=param[list(models.keys())[i]]
+
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
+
+            model.fit(X_train,y_train)
+
+            y_test_pred= model.predict(X_test)
+            y_train_pred= model.predict(X_train)
+
+            test_model_score= r2_score(y_test,y_test_pred)
+            train_model_score=r2_score(y_train,y_train_pred)
+
+            report[list(models.keys())[i]]= test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
